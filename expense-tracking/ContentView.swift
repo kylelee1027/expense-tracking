9564aa56d01f54edd5836936c375e736f642ccfb
kylelee1027 <kylelee1027@gmail.com>
2024-05-30 00:31:10 -0400

    Initialize

---
+++ expense-tracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  expense-tracking
+//
+//  Created by Kyle lee on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ expense-tracking/expense_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackingApp.swift
+//  expense-tracking
+//
+//  Created by Kyle lee on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
